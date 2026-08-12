cc8f45c86762198e12caf461a7bb83cacf493462
agam2468el10-netizen <agam2468el10@gmail.com>
2026-08-12 16:29:47 +0300

    Add files via upload

--- game_field.py
+++ game_field.py
@@ -2,7 +2,6 @@ import contans
 
 matrix = [["SOLDIER BODY" ,"SOLDIER BODY" ,"EMPTY" ,"MINE" ], ["SOLDIER BODY" ,"SOLDIER BODY" ,"MINE" , "EMPTY"], [ "SOLDIER LEG","SOLDIER LEG" , "MINE", "FLAG" ]]
 
-
 def solder_body_place():
     body_place = []
     for row in range(len(matrix)):
@@ -19,17 +18,21 @@ def solder_legs_place():
                 legs_place.append((row,col))
     return legs_place
 
-
 def is_body_on_flag():
     for loc in contans.FLAG_INDEX:
-        for leg in solder_legs_place():
-            if loc == leg:
+        for body in solder_body_place():
+            if loc == body:
                 return True
             else:
-                return False
+                continue
 
-print(is_body_on_flag())
-
+def is_leg_on_mine():
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if matrix[row][col] == contans.MINE:
+                return True
+            else:
+                continue
 
 # def is_win():
 #     if solder_place() in flag_loc():
